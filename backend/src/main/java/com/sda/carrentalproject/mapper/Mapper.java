0523a1afe90092bf830fccfdb854fad580a17f3d
AlmarVasar <almar.vasar@gmail.com>
2023-07-01 13:31:55 +0300

    client scaffolding - finished

---
+++ backend/src/main/java/com/sda/carrentalproject/mapper/Mapper.java
@@ -0,0 +1,11 @@
+package com.sda.carrentalproject.mapper;
+
+import com.sda.carrentalproject.domain.Client;
+import com.sda.carrentalproject.dto.ClientDto;
+
+public interface Mapper<E, D> {
+
+    D fromEntityToDto(E entity);
+
+    E fromDtoToEntity(D dto);
+}
--- backend/src/main/java/com/sda/carrentalproject/service/ClientService.java
+++ backend/src/main/java/com/sda/carrentalproject/service/ClientService.java
@@ -5,6 +5,8 @@ import com.sda.carrentalproject.repository.ClientRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
 public class ClientService {
@@ -14,4 +16,16 @@ public class ClientService {
     public ClientService(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
     }
+
+    public List<Client> getAllClients() {
+        log.info("getting all clients from repository");
+
+        var result = clientRepository.findAll();
+
+        log.info("found [{}] clients", result.size());
+        log.debug("results : {}", result);
+
+        return result;
+    }
 }
+
