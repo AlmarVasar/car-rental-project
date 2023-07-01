adfe74fabcec6673000bd15048614b645669a63e
AlmarVasar <almar.vasar@gmail.com>
2023-07-01 12:05:36 +0300

    Client data - in progress

---
+++ backend/src/main/java/com/sda/carrentalproject/dto/ClientDto.java
@@ -0,0 +1,27 @@
+package com.sda.carrentalproject.dto;
+
+import jakarta.persistence.Table;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Builder
+public record ClientDto(  Long id,
+
+         String name,
+
+         String surname,
+
+         String phone,
+
+         String email,
+
+         String address,
+
+         boolean hasDrivingLicense,
+
+         LocalDateTime registrationDateTime,
+
+         LocalDate dateOfBirth) {
+}
