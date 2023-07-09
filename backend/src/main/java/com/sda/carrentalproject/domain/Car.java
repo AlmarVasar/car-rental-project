ce49eb9ccf8044e1ef38086c940ca96a660c193c
AlmarVasar <almar.vasar@gmail.com>
2023-07-09 13:38:39 +0300

    Car entity created

---
+++ backend/src/main/java/com/sda/carrentalproject/domain/Car.java
@@ -0,0 +1,35 @@
+package com.sda.carrentalproject.domain;
+
+import com.sda.carrentalproject.domain.enumeration.Color;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.YearMonth;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table( name = "Cars")
+//TODO : homework!  implement on java and angular side
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String Brand;
+
+    private String Model;
+
+    private YearMonth productionYear;
+
+    private Color color;
+
+
+}
