397027570390ad622b040e50100f640c73516bb5
AlmarVasar <almar.vasar@gmail.com>
2023-07-09 13:17:40 +0300

    connecting client form to java backend

--- frontend/src/app/components/clients/clients.component.ts
+++ frontend/src/app/components/clients/clients.component.ts
@@ -1,17 +1,17 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {MatSort} from "@angular/material/sort";
 import {MatPaginator} from "@angular/material/paginator";
 import {ClientService} from "../../services/client/client.service";
 import {Client} from "../../models/client";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
-
   displayedColumns: Array<string> =
     ['id', 'name', 'surname', 'phone', 'email', 'address', 'hasDrivingLicense', 'registrationDateTime', 'dateOfBirth'];
   dataSource: MatTableDataSource<Client>;
@@ -21,23 +21,26 @@ export class ClientsComponent implements OnInit {
   @ViewChild(MatSort) sort!: MatSort;
 
   clientForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    surname: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    phone: new FormControl('', [Validators.required, Validators.minLength(7)]),
-    email: new FormControl('', [Validators.required, Validators.email]),
+    name: new FormControl('',
+      [Validators.required, Validators.minLength(2)]),
+    surname: new FormControl('',
+      [Validators.required, Validators.minLength(2)]),
+    phone: new FormControl('',
+      [Validators.required, Validators.minLength(7)]),
+    email: new FormControl('', [
+      Validators.required, Validators.email]),
     address: new FormControl(''),
     hasDrivingLicense: new FormControl(false, Validators.required),
     registrationDateTime: new FormControl(null),
-    dateOfBirth: new FormControl('YYYY-MM-DD', [Validators.required, Validators.minLength(10)])
+    dateOfBirth: new FormControl('YYYY-MM-DD',
+      [Validators.required, Validators.minLength(10)])
   });
-
-
 
   constructor(
     private clientService: ClientService
   ) {
     this.dataSource = new MatTableDataSource(this.clients);
-    console.log("Logging");
+    console.log('inside clients component constructor');
   }
 
   get name() {
@@ -53,15 +56,17 @@ export class ClientsComponent implements OnInit {
   }
 
   get email() {
-  return this.clientForm.controls.email;
-}
+    return this.clientForm.controls.email;
+  }
 
   get address() {
     return this.clientForm.controls.address;
   }
+
   get drivingLicense() {
     return this.clientForm.controls.hasDrivingLicense;
   }
+
   get dateOfBirth() {
     return this.clientForm.controls.dateOfBirth;
   }
@@ -69,16 +74,22 @@ export class ClientsComponent implements OnInit {
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+    this.fetchClients();
+  }
+
+  private fetchClients() {
     this.clientService.getAllClients()
       .subscribe(clients => {
         this.clients = clients;
-        this.dataSource = new MatTableDataSource(this.clients);
+        this.dataSource.data = this.clients;
         console.log(`results: ${JSON.stringify(clients, null, 2)}`);
       });
   }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
@@ -86,6 +97,11 @@ export class ClientsComponent implements OnInit {
 
   onSubmit() {
     console.log(`data to send: ${JSON.stringify(this.clientForm.value, null, 2)}`);
-  }
 
+    this.clientService.createClient(this.clientForm.value as Client)
+      .subscribe(value => {
+        this.clientForm.reset();
+        this.fetchClients();
+      });
+  }
 }
